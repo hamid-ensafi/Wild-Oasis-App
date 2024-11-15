da9c5a11c6f0a21fd77bee10616a0ac743650576
hamid-ensafi <hamidensafi1380.a@gmail.com>
2024-11-16 03:04:39 +0330

    initiall project

---
+++ src/app/cabins/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title:' cabins'
+    
+  };
+
+export default function Page() {
+    return (
+        <h1>cabins page</h1>
+    )
+}
